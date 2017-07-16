6cc55fd39f5960a250015b97913f870557c280a0
daliyajohn <daliyajohn75@gmail.com>
2017-07-16 11:07:53 +0530

    Introduction to JavaScript

---
+++ index.js
@@ -0,0 +1,31 @@
+
+// Introduction to JavaScript
+
+// TYPES
+var myString = undefined;
+var myNumber = undefined;
+var myBoolean = undefined;
+console.log("Name: " + myString);
+console.log("Lucky Number: " + myNumber);
+console.log("Good joke? " + myBoolean);
+
+// We can ask JavaScript to print words to the console with this line of code
+console.log("daliya");
+
+// JavaScript includes the general math operators that you can find on a calculator:
+console.log(3.5+24);
+console.log(2017-1969);
+console.log(65/240);
+console.log(0.2708333333333333*100);
+
+ // generate a random number within a program
+ console.log(Math.floor(Math.random() * 100));
+
+// Variables 
+var strength = '50,000 pounds';
+var age = 24;
+var hasPet = true;
+console.log(age);
+console.log('How much stuff can a variable hold? ', strength);
+console.log(hasPet);
+
